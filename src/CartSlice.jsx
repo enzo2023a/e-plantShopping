4bac583bc69ede8b93aff77da9199e702e1e87ec
enzo23a <enzoaguirre2021@gmail.com>
2025-09-26 23:35:44 -0300

    agrego funcionalidades

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,31 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload; // Desestructurar los datos del producto desde el payload de la acción
+      // Verificar si el producto ya existe en el carrito comparando los nombres
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        // Si el producto ya existe en el carrito, aumentar su cantidad
+        existingItem.quantity++;
+      } else {
+        // Si el producto no existe, agregarlo al carrito con cantidad 1
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
+
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload; // Desestructurar el nombre del producto y la nueva cantidad desde el payload de la acción
+      // Buscar el producto en el carrito que coincida con el nombre dado
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity; // Si se encuentra el producto, actualizar su cantidad con el nuevo valor
+      }
 
-    
+
+
     },
   },
 });
